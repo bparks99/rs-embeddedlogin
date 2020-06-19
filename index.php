aaacd375bd9906a68a6365767dad8c4e3c048f61
bparks99 <46975450+bparks99@users.noreply.github.com>
2020-06-18 21:26:48 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -432,7 +432,7 @@
 		payload = payload + ',"confirmPassword" : "' + document.getElementById("sfid-cpassword").value + '"';
 		payload = payload + ',"startUrl" : "' + encodeURIComponent(SFIDWidget.config.authorizeURL) + '"';
 		payload = payload + ',"includePassword" : true}';
-		
+		alert(JSON.stringify(payload));
 		var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
