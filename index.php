3546fb4d1c070f041716b7a048e880d22507312c
bparks99 <46975450+bparks99@users.noreply.github.com>
2020-08-19 12:38:02 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -912,12 +912,12 @@
 		};
 		xhttp.open("POST", "https://devtom-externalidentity.cs45.force.com/participants/services/apexrest/NewPassword/V1/", true);
 		//xhttp.withCredentials = true;
-		xhttp.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+		xhttp.setRequestHeader("Content-Type", "application/json");
 		xhttp.setRequestHeader("crossOrigin", "true");
 		xhttp.setRequestHeader("Access-Control-Allow-Origin", "https://rs-embeddedlogin.herokuapp.com");
 		xhttp.setRequestHeader("Access-Control-Allow-Methods", "GET, POST");
 		xhttp.setRequestHeader("Access-Control-Allow-Headers", "Origin, Access-Control-Allow-Headers, Content-Type, Authorization, X-Requested-With, X-Auth-Token, Access-Control-Allow-Credentials");
-		xhttp.setRequestHeader('Authorization', 'OAuth ' + SFIDWidget.openid_response.access_token);
+		xhttp.setRequestHeader('Authorization', 'Bearer ' + SFIDWidget.openid_response.access_token);
 		
 		xhttp.send(payload);		
 	}
